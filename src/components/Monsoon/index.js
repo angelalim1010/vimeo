434a23766c577b73dc9e155e0a85bd6146bca9ec
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-11-19 23:23:32 -0500

    updating css to make the layour more responsive

--- src/components/Monsoon/index.js
+++ src/components/Monsoon/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import './Monsoon.scss'
 
 
 function Monsoon() {
   return (
-    <div className="monsoon-container">
-      <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg" alt="monsoon" className = "monsoon-img"/>
-      <div className = "text-container">
+    <div className="row">
+      <div >
+        <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg" alt="monsoon" className = "col-12 col-s-1"/>
+      </div>
+      
+      <div className = "col-6 col-s-1">
         <h1>MONSOON III</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>    
       </div>
--- src/components/Move/index.js
+++ src/components/Move/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import './Move.scss'
 
 
 function Move() {
   return (
-    <div className="move-container">
-      <img src="https://i.vimeocdn.com/video/590587169_530x315.jpg" alt="move" className = "move-img"/>
-      <div className = "text-container">
+    <div className="row">
+      <div>
+        <img src="https://i.vimeocdn.com/video/590587169_530x315.jpg" alt="move" className = "col-12 col-s-12"/>
+      </div>
+      <div className = "col-6 col-s-1">
         <h1>Move 2</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>    
       </div>
